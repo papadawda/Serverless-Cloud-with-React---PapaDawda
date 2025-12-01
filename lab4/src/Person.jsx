d283d22de608d0641450b884dd49511b9af20040
Dawda Njai <dknjai1@madisoncollege.edu>
2025-12-01 01:39:42 -0600

    Save

--- lab4/src/Person.jsx
+++ lab4/src/Person.jsx
@@ -7,20 +7,20 @@ const Person = ({person}) => {
     return (
         <div className="person">
 
-        <h2>Hello, {name}</h2>
-        <p>{name}'s favorite number is {favoriteNumber}</p>
-        <p>{name}'s favorite number plus 10 is {favoriteNumber + 10}</p>
-        <p>{name}'s favorite colors are:</p>
-        <ul>
-            {favoriteColors ?
-                favoriteColors.map(color => (
-                    <li key={color}>{color}</li>
-                ))
-                :
-                <li>None</li>
-            }
+            <h2>Hello, {name}</h2>
+            <p>{name}'s favorite number is {favoriteNumber}</p>
+            <p>{name}'s favorite number plus 10 is {favoriteNumber + 10}</p>
+            <p>{name}'s favorite colors are:</p>
+            <ul>
+                {favoriteColors ?
+                    favoriteColors.map(color => (
+                        <li key={color}>{color}</li>
+                    ))
+                    :
+                    <li>None</li>
+                }
 
-        </ul>
+            </ul>
 
         </div>
     )
