5f72250e65d4d5221d253b8db1251af5387963cd
Dawda Njai <dknjai1@madisoncollege.edu>
2025-12-05 02:33:49 -0600

    starting the react side of the cryptoapp

--- cryptoapp/src/App.jsx
+++ cryptoapp/src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// Import useState and useEffect hooks from React
+import { useState, useEffect } from 'react'
+
+// Import the API category from AWS Amplify
+import { get } from 'aws-amplify/api'
+
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  // Create coins variable and set to empty array
+  const [coins, updateCoins] = useState([])
+
+  // Define function to all API
+  async function fetchCoins() {
+    const request = get({
+      apiName: 'cryptoapi',
+      path: '/coins'
+  })
+    const response = await request.response;
+    const data = await response.body.json();
+    console.log('data is', data);
+    updateCoins(data.coins)
+  }
+
+  // Call fetchCoins function when component loads
+  useEffect(() => {
+    fetchCoins()
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      {
+        coins.map((coin, index) => (
+          <div key={index}>
+            <h2>{coin.name} - {coin.symbol}</h2>
+            <h5>${coin.price_usd}</h5>
+          </div>
+        ))
+      }
+    </div>
+  );
 }
 
 export default App
